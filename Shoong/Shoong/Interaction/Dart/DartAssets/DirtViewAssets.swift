9a29266b334995e34c69cd7d135ff678896b28ee
Suppppppp <u1316@daum.net>
2023-07-27 16:28:05 +0900

    [Setting] 누락된 DartAssets에 경로를 위한 파일 추가

---
+++ Shoong/Shoong/Interaction/Dart/DartAssets/DirtViewAssets.swift
@@ -0,0 +1,20 @@
+//
+//  DirtViewAssets.swift
+//  Shoong
+//
+//  Created by Sup on 2023/07/27.
+//
+
+import SwiftUI
+
+struct DirtViewAssets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DirtViewAssets_Previews: PreviewProvider {
+    static var previews: some View {
+        DirtViewAssets()
+    }
+}

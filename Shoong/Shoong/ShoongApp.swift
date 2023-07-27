11664c153b888888b70ebfb2c4fa9f23e07dea0d
Suppppppp <u1316@daum.net>
2023-07-28 06:29:43 +0900

    [Feat] Template 내용편집화면 추가

--- Shoong/Shoong/ShoongApp.swift
+++ Shoong/Shoong/ShoongApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct ShoongApp: App {
+    
+    // Template 내용작성을 위한 Viewmodel
+    @StateObject var model = TemplatesEditorViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(model)
         }
     }
 }

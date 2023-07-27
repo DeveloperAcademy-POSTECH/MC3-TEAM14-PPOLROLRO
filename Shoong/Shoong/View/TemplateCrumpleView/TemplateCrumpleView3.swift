6d26fcd1ad65770811793a7f1cd6179513e9d4fb
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-07-28 05:05:32 +0900

    디자인팀 플로우 변경으로 연기
    디자인팀 플로우 변경으로 연기

---
+++ Shoong/Shoong/View/TemplateCrumpleView/TemplateCrumpleView3.swift
@@ -0,0 +1,20 @@
+//
+//  TemplateCrumpleView3.swift
+//  Shoong
+//
+//  Created by Zerom on 2023/07/28.
+//
+
+import SwiftUI
+
+struct TemplateCrumpleView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TemplateCrumpleView3_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateCrumpleView3()
+    }
+}

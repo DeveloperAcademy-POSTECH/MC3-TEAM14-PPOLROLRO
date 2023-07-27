b8686fb0c38fff397540d39a1fb21194a1c8f7bd
Suppppppp <u1316@daum.net>
2023-07-27 14:34:16 +0900

    [Setting] Re: Xcode경로설정+ 테마업로드

---
+++ Shoong/Shoong/View/DirTestView.swift
@@ -0,0 +1,22 @@
+//
+//  DirTestView.swift
+//  Shoong
+//
+//  Created by Sup on 2023/07/27.
+//
+
+import SwiftUI
+
+struct DirTestView: View {
+    var body: some View {
+        
+        // 경로 테스트를 위한 가짜 뷰
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DirTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        DirTestView()
+    }
+}

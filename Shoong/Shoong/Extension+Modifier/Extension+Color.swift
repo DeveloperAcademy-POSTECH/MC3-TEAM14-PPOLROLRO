31da0520dd2fe2cb16204ec02c698df8ef1fe93e
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-07-27 15:55:12 +0900

    ColorAssets 및 Extension+Color 파일 추가
    - ColorAssets 추가 및 폴더링
    - Extension+Color 파일 추가 및 정의

--- Shoong/Shoong/Extension+Modifier/Extension+Color.swift
+++ Shoong/Shoong/Extension+Modifier/Extension+Color.swift
@@ -5,4 +5,24 @@
 //  Created by Sup on 2023/07/27.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    // 배경색
+    static let backGroundBeige = Color("backGroundBeige")
+    static let backGroundGray = Color("backGroundGray")
+    static let backGroundYellow = Color("backGroundYellow")
+    
+    // 폴더색
+    static let pointGreen = Color("pointGreen")
+    static let pointOrange = Color("pointOrange")
+    static let pointYellow = Color("pointYellow")
+    static let pointGray = Color("pointGray")
+    
+    // 글자색
+    static let fontOrange = Color("fontOrange")
+    static let fontGray = Color("fontGray")
+    
+    // 인터렉션 선택 카드색
+    static let interactionCardBeige = Color("interactionCardBeige")
+}

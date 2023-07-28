4293c13a926ebce8b0fdedc63990779a24e453fe
금가경(Miya) <100858136+ge-um@users.noreply.github.com>
2023-07-28 16:38:08 +0900

    Revert "InteractionSelectView 추가 및 레이아웃 작업"

--- Shoong/Shoong/ContentView.swift
+++ Shoong/Shoong/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TestNaviView()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 

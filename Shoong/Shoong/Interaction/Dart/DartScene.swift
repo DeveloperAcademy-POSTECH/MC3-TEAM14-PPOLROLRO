477acc29d5a1ff527bd7927f3b294eb2f7e62ded
금가경 <gageum0@gmail.com>
2023-08-01 04:42:01 +0900

    [Fix] 다트판과 충돌 시 꽂히는 효과 구현하기

--- Shoong/Shoong/Interaction/Dart/DartScene.swift
+++ Shoong/Shoong/Interaction/Dart/DartScene.swift
@@ -12,92 +12,113 @@ import SwiftUI
 // SKPhysicsContactDelegate : 충돌 감지에 필요한 프로토콜
 final class DartScene: SKScene, SKPhysicsContactDelegate {
     
-     // 화면 관련 변수
-     let screenWidth = UIScreen.main.bounds.width
-     let screenHeight = UIScreen.main.bounds.height
-     
-     // 노드 관련 변수
-     var dart : SKSpriteNode!
-     var dartboard: SKSpriteNode!
-
-     // 가속도 관련 변수
-     private var motionManager = CMMotionManager()
-     private var previousAcceleration: CMAcceleration?
-
-     override func didMove(to view: SKView) {
-         backgroundColor = UIColor(.backGroundBeige)
-
-         setUpPhysicsWorld()
-         createDart()
-         createDartboard()
-         startDarting()
-     }
-
-     // 물리 세계 설정 : 중력 0
-     func setUpPhysicsWorld() {
-         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-     }
-
-     // 다트보드 생성
-     func createDartboard() {
-         dartboard = SKSpriteNode(imageNamed: "dart_wall")
-         dartboard.position = CGPoint(x: screenWidth / 2, y: 702)
-
-         dartboard.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: dartboard.size.width, height: dartboard.size.height))
-         dartboard.physicsBody?.isDynamic = false
-
-         addChild(dartboard)
-     }
-
-     // 다트 생성
-     func createDart() {
-         dart = SKSpriteNode(imageNamed: "jiggy_02")
-         dart.position = CGPoint(x: screenWidth / 2, y: 100)
-
-         dart.physicsBody?.affectedByGravity = false
-         dart.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: dart.size.width, height: dart.size.height))
-
-         addChild(dart)
-     }
-
-     // 다트 게임 시작
-     private func startDarting() {
-         if motionManager.isAccelerometerAvailable {
-             motionManager.accelerometerUpdateInterval = 0.1
-             motionManager.startAccelerometerUpdates(to: .main) { [weak self] (data, error) in
-                 guard let acceleration = data?.acceleration else { return }
-
-                 if let accelerationChange = self?.calculateAccelerationChange(currentAcceleration: acceleration) {
-
-                     if accelerationChange > 1.2 {
-                         self?.startMotionManager(acceleration: acceleration)
-                     }
-                 }
-             }
-         }
-     }
-
-     // 핸드폰 움직임의 가속도를 받아오는 함수
-     private func calculateAccelerationChange(currentAcceleration: CMAcceleration) -> Double? {
-         guard let previousAcceleration = previousAcceleration else {
-             self.previousAcceleration = currentAcceleration
-             return nil
-         }
-
-         let deltaZ = currentAcceleration.z - previousAcceleration.z
-         let accelerationChange = sqrt(deltaZ * deltaZ)
-
-         self.previousAcceleration = currentAcceleration
-
-         return accelerationChange
-     }
-
-     // 움직임 감지 시 가속도 추가
-     private func startMotionManager(acceleration: CMAcceleration) {
-         guard motionManager.isAccelerometerAvailable else {
-             return
-         }
-         dart.physicsBody?.isDynamic = true
-         dart.physicsBody?.applyImpulse(CGVector(dx: CGFloat(acceleration.y) * 30, dy: 100))
-     }
- }
+    // 화면 관련 변수
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    // 노드 관련 변수
+    var dart : SKSpriteNode!
+    var dartboard: SKSpriteNode!
+    
+    // 가속도 관련 변수
+    private var motionManager = CMMotionManager()
+    private var previousAcceleration: CMAcceleration?
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(.backGroundBeige)
+        
+        setUpPhysicsWorld()
+        createDart()
+        createDartboard()
+        startDarting()
+    }
+    
+    // 물리 세계 설정 : 중력 0
+    func setUpPhysicsWorld() {
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+    }
+    
+    // 다트보드 생성
+    func createDartboard() {
+        dartboard = SKSpriteNode(imageNamed: "dart_wall")
+        dartboard.position = CGPoint(x: screenWidth / 2, y: 702)
+        dartboard.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: dartboard.size.width, height: dartboard.size.height))
+        dartboard.physicsBody?.isDynamic = false
+        dartboard.physicsBody?.categoryBitMask = PhysicsCategory.dartboard
+        
+        addChild(dartboard)
+    }
+    
+    // 다트 생성
+    func createDart() {
+        dart = SKSpriteNode(imageNamed: "jiggy_02")
+        
+        dart.physicsBody?.affectedByGravity = false
+        dart.position = CGPoint(x: screenWidth / 2, y: 100)
+        dart.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: dart.size.width, height: dart.size.height))
+        dart.physicsBody?.categoryBitMask = PhysicsCategory.dart
+        dart.physicsBody?.contactTestBitMask = PhysicsCategory.dartboard
+        dart.physicsBody?.collisionBitMask = PhysicsCategory.dartboard
+        
+        addChild(dart)
+    }
+    
+    
+    // 다트 게임 시작
+    private func startDarting() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.1
+            motionManager.startAccelerometerUpdates(to: .main) { [weak self] (data, error) in
+                guard let acceleration = data?.acceleration else { return }
+                
+                if let accelerationChange = self?.calculateAccelerationChange(currentAcceleration: acceleration) {
+                    
+                    if accelerationChange > 1.2 {
+                        self?.startMotionManager(acceleration: acceleration)
+                    }
+                }
+            }
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var collideBody = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            collideBody = contact.bodyB
+        } else {
+            collideBody = contact.bodyA
+        }
+        
+        // 다트가 다트판에 꽂혔을 시 실행할 행동
+        if collideBody.categoryBitMask == PhysicsCategory.dartboard {
+            print("target!")
+            dart.physicsBody?.linearDamping = 1
+            dart.physicsBody?.isDynamic = false
+        }
+    }
+    
+    // 핸드폰 움직임의 가속도를 받아오는 함수
+    private func calculateAccelerationChange(currentAcceleration: CMAcceleration) -> Double? {
+        guard let previousAcceleration = previousAcceleration else {
+            self.previousAcceleration = currentAcceleration
+            return nil
+        }
+        
+        let deltaZ = currentAcceleration.z - previousAcceleration.z
+        let accelerationChange = sqrt(deltaZ * deltaZ)
+        
+        self.previousAcceleration = currentAcceleration
+        
+        return accelerationChange
+    }
+    
+    // 움직임 감지 시 가속도 추가
+    private func startMotionManager(acceleration: CMAcceleration) {
+        guard motionManager.isAccelerometerAvailable else {
+            return
+        }
+        dart.physicsBody?.isDynamic = true
+        dart.physicsBody?.applyImpulse(CGVector(dx: CGFloat(acceleration.y) * 30, dy: 100))
+    }
+}
---
+++ Shoong/Shoong/Interaction/Dart/PhysicsCategory.swift
@@ -0,0 +1,13 @@
+//
+//  PhysicsCategory.swift
+//  Shoong
+//
+//  Created by 금가경 on 2023/08/01.
+//
+
+import SpriteKit
+
+ struct PhysicsCategory {
+     static let dart: UInt32 = 0x1 << 0 // 1
+     static let dartboard: UInt32 = 0x1 << 1 // 2
+ }

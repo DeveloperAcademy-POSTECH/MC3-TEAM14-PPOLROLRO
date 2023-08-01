57f920e2f7212527229f9fcb1bcd133226706edc
금가경 <gageum0@gmail.com>
2023-08-02 01:10:12 +0900

    [Feat]  버튼 컴포넌트 만들기

---
+++ Shoong/Shoong/Interaction/Dart/HowToPlay.swift
@@ -0,0 +1,34 @@
+//
+//  HowToPlay.swift
+//  Shoong
+//
+//  Created by 금가경 on 2023/08/02.
+//
+
+import SwiftUI
+
+struct HowToPlay: View {
+    let playImageName : String
+    @Binding var isMessagePresented : Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .ignoresSafeArea()
+                .opacity(0.5)
+            VStack(spacing: 18) {
+                Image(playImageName)
+                Button(action: {
+                    isMessagePresented = false
+                }, label: {
+                    Image("xmark")})
+            }
+        }
+    }
+}
+
+struct HowToPlay_Previews: PreviewProvider {
+    static var previews: some View {
+        HowToPlay(playImageName: "dartHowToPlay", isMessagePresented: .constant(true))
+    }
+}

2df8225d1acb95de1ba1eefc72f0e2fd7005edd7
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-08-02 01:37:16 +0900

    인터렉션 트렌지션 뷰 1, 2 추가 및 수정
    - 인터렉션 트렌지션 뷰 1, 2 추가 및 수정
    - Navigation 연결

--- Shoong/Shoong/View/TemplateCrumpleView/TemplateCrumpleView1.swift
+++ Shoong/Shoong/View/TemplateCrumpleView/TemplateCrumpleView1.swift
@@ -8,102 +8,161 @@
 import SwiftUI
 
 struct TemplateCrumpleView1: View {
-    @State private var isNavigation: Bool = false
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var isInteraction: Bool = false
     @State private var draggedOffset: CGFloat = 50
     @State private var accumulatedOffset: CGFloat = 0.0
-    @State private var textOpacity: Double = 1.0
+    @State private var isScriptCheck: Bool = false
+    
+    @Binding var firstNaviLinkActive: Bool
+    
+    let width: CGFloat = UIScreen.main.bounds.width
+    let height: CGFloat = UIScreen.main.bounds.height
     
     var body: some View {
         ZStack(alignment: .top) {
             Color.backGroundBeige.ignoresSafeArea()
             
-            VStack {
-                Image("crumpleFolder")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: UIScreen.main.bounds.width * 0.9)
-                
-                ZStack(alignment: .top) {
-                    Image("testTemplate")
+            ZStack {
+                ZStack {
+                    Image("scriptFolder")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: UIScreen.main.bounds.width * 0.65)
+                    
+                    VStack {
+                        HStack {
+                            Text("1/4")
+                            
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                        
+                        if isScriptCheck {
+                            Text("하지만, 쉽지 않다는 것을\n잘 아는 당신은...")
+                                .multilineTextAlignment(.center)
+                        } else {
+                            Text("당신은 드디어, 사직서를\n봉투에 담았다!")
+                                .multilineTextAlignment(.center)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            NavigationLink {
+                                InteractionSelectView(firstNaviLinkActive: $firstNaviLinkActive)
+                            } label: {
+                                Text("건너뛰기")
+                            }
+                            
+                            Spacer()
+                            
+                            if isScriptCheck {
+                                NavigationLink {
+                                    TemplateCrumpleView2(firstNaviLinkActive: $firstNaviLinkActive)
+                                } label: {
+                                    Text("다음")
+                                        .bold()
+                                }
+                                
+                            } else {
+                                Button {
+                                    isScriptCheck.toggle()
+                                } label: {
+                                    Text("다음")
+                                        .bold()
+                                }
+                            }
+                        }
+                    }
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 30)
+                }
+                .font(.system(size: 17))
+                .foregroundColor(.fontRed)
+                .frame(width: width - 32, height: (width - 32) * 298 / 358)
+                .opacity(isInteraction ? 1 : 0)
+                .offset(y: -height * 0.05)
+                
+                ZStack(alignment: .top) {
+                    Image("PartiallyEditableTemplate0")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width - 40)
                         .zIndex(1)
                         .offset(y: draggedOffset)
                         .gesture(
                             DragGesture()
-                              .onChanged { gesture in
-                                  if gesture.translation.height > 0 {
-                                      draggedOffset = accumulatedOffset + gesture.translation.height
-                                      
-                                      textOpacity = 0.0
-                                  }
-                              }
-                              .onEnded { gesture in
-                                  accumulatedOffset += gesture.translation.height
-                                  
-                                  if draggedOffset > UIScreen.main.bounds.height / 3 {
-                                      print("@Log")
-                                      draggedOffset = UIScreen.main.bounds.height / 2
-                                      
-                                      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                          isNavigation.toggle()
-                                      }
-                                  }
-                              }
+                                .onChanged { gesture in
+                                    if !isInteraction && gesture.translation.height > 0 {
+                                        draggedOffset = accumulatedOffset + gesture.translation.height
+                                    }
+                                }
+                                .onEnded { gesture in
+                                    if !isInteraction {
+                                        accumulatedOffset += gesture.translation.height
+                                        
+                                        if draggedOffset > UIScreen.main.bounds.height / 3 {
+                                            draggedOffset = UIScreen.main.bounds.height / 2
+                                            
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                                isInteraction.toggle()
+                                            }
+                                        }
+                                    }
+                                }
                         )
-                    
-                    NavigationLink(destination: TemplateCrumpleView2(), isActive: $isNavigation) {
-                        Text(".")
-                            .foregroundColor(.clear)
-                    }
-                    
-                    Text("사직서를 밀어서\n봉투 안으로 넣어보세요.")
-                        .multilineTextAlignment(.center)
-                        .font(.title3)
-                        .zIndex(2)
-                        .opacity(textOpacity)
-                        .padding(.top, UIScreen.main.bounds.height / 4)
                     
                     Image("envelopeTop")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: UIScreen.main.bounds.width * 0.66)
+                        .frame(width: width - 32)
                         .padding(.bottom, -10)
-                        .offset(y: UIScreen.main.bounds.height * 0.5)
+                        .offset(y: UIScreen.main.bounds.height * 0.6)
                         .zIndex(0)
                     
                     Image("envelopeBottom")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: UIScreen.main.bounds.width * 0.66)
-                        .offset(y: UIScreen.main.bounds.height * 0.544)
+                        .frame(width: width - 32)
+                        .offset(y: UIScreen.main.bounds.height * 0.66)
                         .zIndex(2)
-                    
-                    Image("downButton")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 72)
-                        .offset(y: UIScreen.main.bounds.height * 0.52)
-                        .zIndex(3)
                 }
             }
         }
+        .navigationBarBackButtonHidden()
         .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    HStack(spacing: 4) {
+                        Image(systemName: "chevron.backward")
+                            .fontWeight(.semibold)
+                        
+                        Text("뒤로")
+                    }
+                    .foregroundColor(.black)
+                }
+            }
+            
             ToolbarItem(placement: .principal) {
-                Text("1/4")
-                    .foregroundColor(.blue)
+                Text("사직서 날리기")
+                    .bold()
             }
         }
-        .onAppear {
+        .onDisappear {
             draggedOffset = 50
             accumulatedOffset = 0
+            isInteraction = false
+            isScriptCheck = false
         }
     }
 }
 
 struct TemplateCrumpleView1_Previews: PreviewProvider {
     static var previews: some View {
-        TemplateCrumpleView1()
+        TemplateCrumpleView1(firstNaviLinkActive: .constant(true))
     }
 }
--- Shoong/Shoong/View/TemplateCrumpleView/TemplateCrumpleView2.swift
+++ Shoong/Shoong/View/TemplateCrumpleView/TemplateCrumpleView2.swift
@@ -8,13 +8,114 @@
 import SwiftUI
 
 struct TemplateCrumpleView2: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding var firstNaviLinkActive: Bool
+    
+    @State private var isScriptCheck: Bool = false
+    
+    let width: CGFloat = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backGroundBeige.ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    Image("scriptFolder")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack {
+                        HStack {
+                            Text("2/4")
+                            
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                        
+                        if isScriptCheck {
+                            Text("참을 수 없는 감정으로\n사직서를 구겨버렸다!")
+                                .multilineTextAlignment(.center)
+                        } else {
+                            Text("놀랍게도, 곧 사직서는\n움직이기 시작하는데...")
+                                .multilineTextAlignment(.center)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            NavigationLink {
+                                InteractionSelectView(firstNaviLinkActive: $firstNaviLinkActive)
+                            } label: {
+                                Text("건너뛰기")
+                            }
+                            
+                            Spacer()
+                            
+                            if isScriptCheck {
+                                NavigationLink {
+                                    TemplateCrumpleView3()
+                                } label: {
+                                    Text("다음")
+                                        .bold()
+                                }
+                                
+                            } else {
+                                Button {
+                                    isScriptCheck.toggle()
+                                } label: {
+                                    Text("다음")
+                                        .bold()
+                                }
+                            }
+                        }
+                    }
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 30)
+                }
+                .font(.system(size: 17))
+                .foregroundColor(.fontRed)
+                .frame(width: width - 32, height: (width - 32) * 298 / 358)
+                .padding(.top, 40)
+                
+                Spacer()
+                
+                Image("paper")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 275)
+                
+                Spacer()
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    HStack(spacing: 4) {
+                        Image(systemName: "chevron.backward")
+                            .fontWeight(.semibold)
+                        
+                        Text("뒤로")
+                    }
+                    .foregroundColor(.black)
+                }
+            }
+            
+            ToolbarItem(placement: .principal) {
+                Text("사직서 날리기")
+                    .bold()
+            }
+        }
     }
 }
 
 struct TemplateCrumpleView2_Previews: PreviewProvider {
     static var previews: some View {
-        TemplateCrumpleView2()
+        TemplateCrumpleView2(firstNaviLinkActive: .constant(true))
     }
 }

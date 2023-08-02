08228074a399b4f34eeb2739b67ec41d0778ba3f
금가경 <gageum0@gmail.com>
2023-08-02 22:10:47 +0900

    [Design] 레이아웃 작업 하기

--- Shoong/Shoong/Interaction/Bowling/BowlingView.swift
+++ Shoong/Shoong/Interaction/Bowling/BowlingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import SpriteKit
 
 struct BowlingView: View {
+    @Environment(\.dismiss) private var dismiss
+
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
 
@@ -18,19 +20,41 @@ struct BowlingView: View {
 
         scene.size = CGSize(width: screenWidth, height: screenHeight)
         scene.scaleMode = .fill
-        scene.backgroundColor = .gray
+        scene.backgroundColor = UIColor(.backGroundBeige)
 
         return scene
 
     }
 
     var body: some View {
-
+        
         SpriteView(scene: myscene)
             .frame(width: screenWidth, height: screenHeight)
             .edgesIgnoringSafeArea(.all)
-
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack(spacing: 4) {
+                            Image(systemName: "chevron.backward")
+                                .font(.custom("SFPro-Semibold", size: 17))
+                            Text("뒤로")
+                                .font(.custom("SFPro-Regular", size: 17))
+                        }
+                        .foregroundColor(.black)
+                    }
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("볼링공 던지기")
+                        .font(.custom("SFPro-Semibold", size: 17))
+                        .tracking(-0.4)
+                        .lineSpacing(20)
+                }
+            }
     }
+    
 }
 
 struct BowlingView_Previews: PreviewProvider {
